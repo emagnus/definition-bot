4fd8ae5ba204f94c8e3fb8f9c0ed640e59734f73
Magnus Westergaard <westerga@gmail.com>
2011-03-25 15:29:43 +0100

    remove silly ud tags that clutter the definitions

--- udscrape.py
+++ udscrape.py
@@ -1,5 +1,6 @@
 from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
+from BeautifulSoup import NavigableString
 from datetime import datetime
 import time
 import urllib2
@@ -16,11 +17,25 @@ def udquery(query):
     """
     query = "+".join(query)
     url = "http://www.urbandictionary.com/define.php/?term="+query
-    soup = BeautifulSoup(fetch(url))
+    soup = BeautifulSoup(fetch(url))    
     try:
-        html = soup.find("div",{"class":"definition"}).contents[0].replace("\r","")
+        html = extract_text(soup.find("div",{"class":"definition"}).contents).replace("\r","")
         text = BeautifulStoneSoup(html,convertEntities=BeautifulStoneSoup.HTML_ENTITIES).contents[0]
         definition = unicode(text).decode("utf-8").encode("utf-8")
     except:
         definition = "Ingen definisjon."
     return definition
+
+def extract_text(contents):
+    if type(contents).__name__ == 'list':
+        s = ""
+        for c in contents:
+            if type(c) != NavigableString:
+                c = extract_text(c.contents)
+            s += unicode(c)
+        return s
+    if type(contents) == NavigableString:
+        return contents
+    
+
+        
